bf54e60245be72e943f4f72a7fbdce88896cbca3
Kaic <kaicsalha@icloud.com>
2026-03-03 19:59:21 -0300

    Add files via upload

---
+++ src/main/java/org/example/ExThrow.java
@@ -0,0 +1,16 @@
+package org.example;
+
+public class ExThrow {
+
+    static void validarIdade(int idade){
+        if (idade <= 18){
+            throw new IllegalArgumentException("Idade minima é 18 !");
+        }
+        System.out.println("Acesso Liberado!");
+    }
+
+    public static void main(String[] args) {
+
+        validarIdade(15);
+    }
+}
